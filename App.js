9acef8f2d35282c1c907189bf2e75009f6353236
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2021-10-12 11:22:35 +0700

    implement shaka player with ivq overlay

--- App.js
+++ App.js
@@ -2,21 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import VideoJS from './VideoJS'
+import ShakaPlayer from './ShakaPlayer';
 
 export default function App() {
   let playerRef = React.useRef(null);
 
   let [isIVQVisible, setIVQVisible] = useState(false);
+  let [isIVQ2Visible, setIVQ2Visible] = useState(false);
 
   let videoJsOptions = { // lookup the options in the docs for more options
     autoplay: false,
     controls: true,
     responsive: true,
     fluid: true,
-    sources: [{
-      src: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-      type: 'video/mp4'
-    }]
+    // sources: [{
+    //   src: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+    //   type: 'video/mp4'
+    // }]
   }
 
   let handlePlayerReady = (player) => {
@@ -24,18 +26,34 @@ export default function App() {
 
     // you can handle player events here
     player.on('waiting', () => {
-      console.log('player is waiting');
+      console.log('videojs player is waiting');
     });
 
     player.on('dispose', () => {
-      console.log('player will dispose');
+      console.log('videojs player will dispose');
     });
   };
 
   return (
     <View style={styles.container}>
-      <VideoJS isIVQVisible={isIVQVisible} options={videoJsOptions} onReady={handlePlayerReady} />
-      <Button title="Toggle IVQ" onPress={() => setIVQVisible(s => !s)} />
+      <View style={{ paddingVertical: 24, width: '100%' }}>
+        <VideoJS 
+          isIVQVisible={isIVQVisible} 
+          options={videoJsOptions} 
+          onReady={handlePlayerReady} 
+        />
+        <Button 
+          title="Toggle IVQ" 
+          onPress={() => setIVQVisible(s => !s)}
+        />
+      </View>
+      <View style={{ paddingVertical: 24 }}>
+        <ShakaPlayer isIVQVisible={isIVQ2Visible} />
+        <Button 
+          title="Toggle IVQ" 
+          onPress={() => setIVQ2Visible(s => !s)}
+        />
+      </View>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
---
+++ ShakaPlayer.js
@@ -0,0 +1,83 @@
+import React, { useEffect, useRef } from 'react'
+import { StyleSheet, View, Text } from 'react-native';
+import shaka from 'shaka-player/dist/shaka-player.ui';
+
+import 'shaka-player/dist/controls.css';
+
+export default function ShakaPlayer(props) {
+  let { isIVQVisible } = props;
+  
+  let videoRef = useRef(null);
+  let uiRef = useRef(null);
+
+  let onError = (e) => console.error('>>shaka event error', e);
+
+  useEffect(() => {
+    let player = new shaka.Player(videoRef.current);
+    let ui = new shaka.ui.Overlay(player, uiRef.current, videoRef.current);
+
+    ui.getControls()
+
+    player.addEventListener('error', onError);
+
+    player.configure({
+      drm: {
+        servers: {
+          'com.widevine.alpha': 'https://wv.service.expressplay.com/hms/wv/rights/?ExpressPlayToken=BQAO5vNIKdgAJDM0NTJhZjc1LWI5ZTUtNDkzMy04NDE0LTc5ZTk1NzA5ZmMxMgAAAGBrzAhfZwl6cSGmIvVk3DjSKGJaMb-viBxomFoRP-4sj4sRxwdnA07mJxm3_tf8Whl_L_BLaDq2hwvh_mkyxlh7R7G2s-QYypBOnPzmIfBK0ORpxGVpX-jRM0pj6DtiufcQp5WNx13QDrH7ksRAWeoZxAH39Q',
+          // 'com.apple.fps.1_0': 'https://fp.service.expressplay.com/hms/fp/rights/?ExpressPlayToken=BgAO5vNIKe8AJDM0NTJhZjc1LWI5ZTUtNDkzMy04NDE0LTc5ZTk1NzA5ZmMxMgAAAGBchs10oXBoQkVFLsVXIOuh4VZeSkvxD13qzOCGHw_le_Ztug9j4o-ZEwT28BmLL_wp07HPx252iDEKGtw8tlts_mqyVzD2PwRU7hCXhrMi45_by93RVtWKnID8Xas_IbSuPszysFjdDfjfpva7NoQ1FXlxeQ',
+          // 'com.microsoft.playready': 'https://pr.service.expressplay.com/playready/RightsManager.asmx?ExpressPlayToken=BwAO5vNIN1AAJDM0NTJhZjc1LWI5ZTUtNDkzMy04NDE0LTc5ZTk1NzA5ZmMxMgAAAJCESaR361tLKCdu8am4MsUkKliFnbB_7RE-CslE_W4OdrhZ53Hu_Wpreg9ay_Dh30SU9l79YSJg1IFv8-7QRLGURNKPQfVRVN8fNjHXl8nFDaGNV2eM3zF-0LbmN0UjLIMRVW8850IMBAC7EkwDvq2ELtqGyUoFrR6PZ9XWcX_1efHTNKXp0NjmeRipKNKAKAF11Yxf17WHgc8PUmmXzsSY_G5N9Q',
+          // 'com.widevine.alpha': 'https://widevine-proxy.appspot.com/proxy',
+        },
+        advanced: {
+          'com.widevine.alpha': {
+            'videoRobustness': 'SW_SECURE_DECODE',
+          },
+        }
+      }
+    })
+    
+    async function loadAsset() {
+      try {
+        // await player.load('http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4')
+        // await player.load('https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd')
+        // await player.load('https://bitmovin-a.akamaihd.net/content/art-of-motion_drm/mpds/11331.mpd')
+        await player.load('https://rubel-video-cdn.ruangguru.com/RGVV-OMKP3UR83RJ/ca037c3e-1aa1-4e8a-9512-6bbae733955b/ca037c3e-1aa1-4e8a-9512-6bbae733955b.mpd')
+        console.log('>>shaka video loaded')
+      } catch(e) {
+        console.error('shaka video catch', e)
+      }
+    }
+
+    loadAsset();
+
+    return () => {
+      player.removeEventListener('error');
+    }
+  }, []);
+
+  return (
+    <div ref={uiRef}>
+      <video
+        ref={videoRef}
+        // controls
+        style={{ width: '100%', maxWidth: '100%' }}
+        {...props}
+      />
+      {isIVQVisible && <View style={styles.ivq}>
+        <Text>Howdy world!</Text>
+      </View>}
+    </div>
+  )
+}
+
+const styles = StyleSheet.create({
+  ivq: {
+    backgroundColor: 'skyblue', 
+    zIndex: 1000, 
+    position: 'absolute', 
+    bottom: 60, 
+    top: 0, 
+    left: 0, 
+    right: 0,
+  }
+})
