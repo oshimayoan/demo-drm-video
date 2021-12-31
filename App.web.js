2cd5a408f6e8afccddbd64e003547ddea5e05135
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2021-12-31 10:53:44 +0700

    wip unity

--- App.web.js
+++ App.web.js
@@ -3,12 +3,26 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import VideoJS from './VideoJS'
 import ShakaPlayer from './ShakaPlayer';
+// import Unity, { UnityContent } from "react-unity-webgl";
+
+// const unityContext = new UnityContext({
+//   loaderUrl: "unityweb/myunityapp.loader.js",
+//   dataUrl: "unityweb/myunityapp.data",
+//   frameworkUrl: "unityweb/myunityapp.framework.js",
+//   codeUrl: "unityweb/myunityapp.wasm",
+// });
+
+// const unityContent = new UnityContent(
+//   "unity/testWEbgl.json",
+//   "unity/UnityLoader.js"
+// );
 
 export default function App() {
   let playerRef = React.useRef(null);
 
   let [isIVQVisible, setIVQVisible] = useState(false);
   let [isIVQ2Visible, setIVQ2Visible] = useState(false);
+  let [isUnity2Visible, setUnity2Visible] = useState(false);
 
   let videoJsOptions = { // lookup the options in the docs for more options
     autoplay: false,
@@ -36,7 +50,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <View style={{ paddingVertical: 24, width: '100%' }}>
+      {/* <View style={{ paddingVertical: 24, width: '100%' }}>
         <VideoJS 
           isIVQVisible={isIVQVisible} 
           options={videoJsOptions} 
@@ -46,15 +60,20 @@ export default function App() {
           title="Toggle IVQ" 
           onPress={() => setIVQVisible(s => !s)}
         />
-      </View>
+      </View> */}
       <View style={{ paddingVertical: 24 }}>
-        <ShakaPlayer isIVQVisible={isIVQ2Visible} />
+        <ShakaPlayer isIVQVisible={isIVQ2Visible} isUnityVisible={isUnity2Visible} />
         <Button 
           title="Toggle IVQ" 
           onPress={() => setIVQ2Visible(s => !s)}
         />
+        <Button
+          title="Toggle Unity"
+          onPress={() => setUnity2Visible(s => !s)}
+        />
       </View>
       <Text>Open up App.js to start working on your app!</Text>
+      {/* <Unity unityContent={unityContent} /> */}
       <StatusBar style="auto" />
     </View>
   );
--- ShakaPlayer.js
+++ ShakaPlayer.js
@@ -1,16 +1,56 @@
-import React, { useEffect, useRef } from 'react'
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react'
+import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
 import shaka from 'shaka-player/dist/shaka-player.ui';
+import Unity, { UnityContent } from "react-unity-webgl";
 
 import 'shaka-player/dist/controls.css';
 
+// const unityContent = new UnityContent(
+//   "unityPassingTest/passingValueTest.json",
+//   "unityPassingTest/UnityLoader.js"
+// );
+
+const unityContent = new UnityContent(
+  'unityStream/unityplayer-webgl.json',
+  'unityStream/UnityLoader.js'
+)
+
 export default function ShakaPlayer(props) {
-  let { isIVQVisible } = props;
+  let { isIVQVisible, isUnityVisible } = props;
+
+  let [hexColour, setHexColour] = useState('#ff0000');
   
   let videoRef = useRef(null);
   let uiRef = useRef(null);
 
+  let onApplyHexColour = () => {
+    console.log('hex colour should be applied')
+    unityContent.send('GameHandler', 'ChangeColour', 'ff0000')
+  }
+
   let onError = (e) => console.error('>>shaka event error', e);
+
+  // useEffect(() => {
+  //   !isUnityVisible && unityContent.remove();
+  // }, [isUnityVisible])
+  
+  useEffect(() => {
+    console.log('>>unity content', unityContent);
+    unityContent.on('progress', () => console.log('unity is on progress'));
+
+    unityContent.on('done', () => console.log('unity is done'));
+    unityContent.on('finished', () => console.log('unity is finished'));
+
+    unityContent.on('started', () => console.log('unity is starting'))
+
+    unityContent.on('quitted', () => console.log('unity is quitted'))
+
+    unityContent.on('ready', () => console.log('unity is ready'))
+
+    unityContent.on('GameOver', () => console.log('unity is game over'))
+
+    unityContent.on('loaded', () => console.log('unity is loaded'))
+  }, [])
 
   useEffect(() => {
     let player = new shaka.Player(videoRef.current);
@@ -20,11 +60,13 @@ export default function ShakaPlayer(props) {
       enableKeyboardPlaybackControls: true,
       controlPanelElements: [
         'play_pause',
+        'quality',
         'time_and_duration',
         'loop',
         'mute',
         'volume',
         'spacer',
+        'quality',
         'cast',
         'fullscreen',
         'overflow_menu',
@@ -32,6 +74,7 @@ export default function ShakaPlayer(props) {
       'overflowMenuButtons': [
         'captions',
         'quality',
+        'play_pause',
         'playback_rate',
       ],
     };
@@ -43,7 +86,7 @@ export default function ShakaPlayer(props) {
     player.configure({
       drm: {
         servers: {
-          'com.widevine.alpha': 'https://wv.service.expressplay.com/hms/wv/rights/?ExpressPlayToken=BQAO5vNIKdgAJDM0NTJhZjc1LWI5ZTUtNDkzMy04NDE0LTc5ZTk1NzA5ZmMxMgAAAGBrzAhfZwl6cSGmIvVk3DjSKGJaMb-viBxomFoRP-4sj4sRxwdnA07mJxm3_tf8Whl_L_BLaDq2hwvh_mkyxlh7R7G2s-QYypBOnPzmIfBK0ORpxGVpX-jRM0pj6DtiufcQp5WNx13QDrH7ksRAWeoZxAH39Q',
+          'com.widevine.alpha': 'https://wv.service.expressplay.com/hms/wv/rights/?ExpressPlayToken=BQAO5vNIKdAAJDM0NTJhZjc1LWI5ZTUtNDkzMy04NDE0LTc5ZTk1NzA5ZmMxMgAAAGDrOqG_FEarVb8rn4BJId2iOsB3D38oDRXMVVvfyOqUgQ3iyNQCqwNfrAAWCvcf01DyM0fEe3r6TuZzQc10Qc0YRI2-HI3BhDW_uam1TIbNYRPTaNvUgl__ar-4bFlUTY_0s0CaNM-ejZQTvpovIZZWp_Vohg',
           // 'com.apple.fps.1_0': 'https://fp.service.expressplay.com/hms/fp/rights/?ExpressPlayToken=BgAO5vNIKe8AJDM0NTJhZjc1LWI5ZTUtNDkzMy04NDE0LTc5ZTk1NzA5ZmMxMgAAAGBchs10oXBoQkVFLsVXIOuh4VZeSkvxD13qzOCGHw_le_Ztug9j4o-ZEwT28BmLL_wp07HPx252iDEKGtw8tlts_mqyVzD2PwRU7hCXhrMi45_by93RVtWKnID8Xas_IbSuPszysFjdDfjfpva7NoQ1FXlxeQ',
           // 'com.microsoft.playready': 'https://pr.service.expressplay.com/playready/RightsManager.asmx?ExpressPlayToken=BwAO5vNIN1AAJDM0NTJhZjc1LWI5ZTUtNDkzMy04NDE0LTc5ZTk1NzA5ZmMxMgAAAJCESaR361tLKCdu8am4MsUkKliFnbB_7RE-CslE_W4OdrhZ53Hu_Wpreg9ay_Dh30SU9l79YSJg1IFv8-7QRLGURNKPQfVRVN8fNjHXl8nFDaGNV2eM3zF-0LbmN0UjLIMRVW8850IMBAC7EkwDvq2ELtqGyUoFrR6PZ9XWcX_1efHTNKXp0NjmeRipKNKAKAF11Yxf17WHgc8PUmmXzsSY_G5N9Q',
           // 'com.widevine.alpha': 'https://widevine-proxy.appspot.com/proxy',
@@ -76,22 +119,39 @@ export default function ShakaPlayer(props) {
   }, []);
 
   return (
-    <div ref={uiRef}>
-      <video
-        ref={videoRef}
-        // controls
-        style={{ width: '100%', maxWidth: '100%' }}
-        {...props}
-      />
-      {isIVQVisible && <View style={styles.ivq}>
-        <Text>Howdy world!</Text>
-      </View>}
-    </div>
+    <>
+      <div ref={uiRef}>
+        <video
+          ref={videoRef}
+          // controls
+          style={{ width: '100%', maxWidth: '100%' }}
+          {...props}
+        />
+        {isIVQVisible && <View style={styles.overlay}>
+          <Text>Howdy world!</Text>
+        </View>}
+        {isUnityVisible && <View style={styles.overlay}>
+          <Unity unityContent={unityContent} />
+        </View>}
+      </div>
+      {isUnityVisible && <>
+        {/* <TextInput
+          placeholder="Hex colour"
+          value={hexColour}
+          onChangeText={(hex) => setHexColour(hex)}
+          style={{padding: 16, borderWidth: 1}}
+        /> */}
+        <Button
+          title="Apply hex colour"
+          onPress={() => onApplyHexColour()}
+        />
+      </>}
+    </>
   )
 }
 
 const styles = StyleSheet.create({
-  ivq: {
+  overlay: {
     backgroundColor: 'skyblue', 
     zIndex: 1000, 
     position: 'absolute', 
